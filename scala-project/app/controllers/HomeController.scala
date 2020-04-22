89b010c6e981dd1b7a3257a8c8f57f75b3540f5d
Kakaranish <stasiekgruz@gmail.com>
2020-04-22 23:11:40 +0200

    [Project ???][Created simple example of encrypting password with bcrypt use]

--- scala-project/app/controllers/HomeController.scala
+++ scala-project/app/controllers/HomeController.scala
@@ -1,23 +1,40 @@
 package controllers
 
 import javax.inject._
+import play.api.libs.json.{JsObject, JsString, Json}
 import play.api.mvc._
+import com.github.t3hnar.bcrypt._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+import scala.concurrent.ExecutionContext
+
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+class HomeController @Inject()(cc: ControllerComponents)(implicit exec: ExecutionContext) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page with a welcome message.
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  case class Person(firstName: String,
+                    lastName: String,
+                    age: Int)
+  object Person {
+    implicit val personFormat = Json.format[Person]
   }
 
+  def index = Action(parse.json) { implicit request =>
+    val message = (request.body \ "message").asOpt[String]
+    val password = (request.body \ "password").asOpt[String]
+
+    val salt = "$2a$10$8K1p/a0dL1LXMIgoEDFrwO"
+    val encryptedPassword = password.get.bcrypt(salt)
+
+    val validPassword = "testPassword"
+    val passwordValid = validPassword.bcrypt(salt) == encryptedPassword
+
+    val response = JsObject(
+      Seq(
+        "message" -> JsString("Hello world"),
+        "person" -> Json.toJson(Person("Stanislaw", "Gruz", 22)),
+        "encryptedPassword" -> JsString(encryptedPassword),
+        "isPasswordValid" -> Json.toJson(passwordValid)
+      )
+    )
+    Ok(response)
+  }
 }
